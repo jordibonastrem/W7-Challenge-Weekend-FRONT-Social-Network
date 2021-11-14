a5445403dcc992008abf1c51f00e60d56b2ba643
Jordi <jordi.bonastre.m@gmail.com>
2021-11-14 19:12:09 +0100

    getUsersThunk aded, and path folder aded containing all paths

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { userPaths } from "../../paths/paths";
 import { loginUserAction } from "../actions/actionCreators";
 
 export const loginUserThunk = (user) => async (dispatch) => {
   const res = await axios.post(
-    `${process.env.REACT_APP_API_URL}users/login`,
+    process.env.REACT_APP_API_URL + userPaths.loginUser,
     user
   );
 
@@ -22,4 +23,18 @@ export const loginUserThunk = (user) => async (dispatch) => {
       JSON.stringify({ token: token })
     );
   }
+};
+
+export const getUsersThunk = () => async (dispatch) => {
+  const { token } = JSON.parse(
+    localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY)
+  );
+
+  const users = await axios.get(
+    process.env.REACT_APP_API_URL + userPaths.getUsers,
+    {
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+  // dispatch(f)
 };
