c2a9a012d799e813b4bef06ada4f0bbe75fba264
Jordi <jordi.bonastre.m@gmail.com>
2021-11-14 14:57:10 +0100

    test loginAction creator

---
+++ src/redux/actions/actionCreators.test.js
@@ -0,0 +1,25 @@
+import { loginUserAction, logoutUserAction } from "./actionCreators";
+import actionTypes from "./actionTypes";
+
+let user;
+beforeAll(() => {
+  user = {
+    username: "testuser",
+    password: "testpassword",
+  };
+});
+
+describe("Given a loginUserAction", () => {
+  describe("When it recieves a user", () => {
+    test("Then it should return a object containing the actionType loginUser, and the user recieved", () => {
+      const expectedOutput = {
+        type: actionTypes.loginUser,
+        user,
+      };
+
+      const result = loginUserAction(user);
+
+      expect(result).toEqual(expectedOutput);
+    });
+  });
+});
