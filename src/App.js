8092e278aba0f144fbcbe9b58580ec5e42c28f97
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-15 11:31:29 +0100

    fixed imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import LoginPage from "./pages/LoginPage/LoginPage";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
 
 const App = () => {
   return <LoginPage></LoginPage>;
